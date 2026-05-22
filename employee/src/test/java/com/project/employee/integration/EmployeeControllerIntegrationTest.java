7fa63779a105b247341e174080cb89b6788bd09d
Anil <anilkumargodavarthi136@gmail.com>
2026-05-22 17:53:49 +0530

    Added Junit & Integration tests

--- employee/src/test/java/com/project/employee/integration/EmployeeControllerIntegrationTest.java
+++ employee/src/test/java/com/project/employee/integration/EmployeeControllerIntegrationTest.java
@@ -1,11 +1,17 @@
 package com.project.employee.integration;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 @SpringBootTest(
         webEnvironment =
@@ -19,15 +25,140 @@ class EmployeeControllerIntegrationTest {
     @Autowired
     private TestRestTemplate restTemplate;
 
+    // Health API Test
     @Test
-    void testEmployeesApi() {
+    void testHealthApi() {
 
         String response =
                 restTemplate.getForObject(
-                        "http://localhost:" + port + "/api/employees",
+                        "http://localhost:" + port +
+                                "/api/employees/health",
                         String.class
                 );
 
-        assertTrue(response.contains("Anil"));
+        assertTrue(
+                response.contains("Running")
+        );
+    }
+
+    // Get All Employees Test
+    @Test
+    void testGetEmployeesApi() {
+
+        String response =
+                restTemplate.getForObject(
+                        "http://localhost:" + port +
+                                "/api/employees",
+                        String.class
+                );
+
+        assertTrue(
+                response.contains("Anil")
+        );
+    }
+
+    // Get Employee By ID Test
+    @Test
+    void testGetEmployeeByIdApi() {
+
+        String response =
+                restTemplate.getForObject(
+                        "http://localhost:" + port +
+                                "/api/employees/1",
+                        String.class
+                );
+
+        assertTrue(
+                response.contains("1")
+        );
+    }
+
+    // Add Employee Test
+    @Test
+    void testAddEmployeeApi() {
+
+        HttpHeaders headers =
+                new HttpHeaders();
+
+        HttpEntity<String> request =
+                new HttpEntity<>(null, headers);
+
+        ResponseEntity<String> response =
+                restTemplate.postForEntity(
+                        "http://localhost:" + port +
+                                "/api/employees",
+                        request,
+                        String.class
+                );
+
+        assertEquals(
+                HttpStatus.OK,
+                response.getStatusCode()
+        );
+
+        assertTrue(
+                response.getBody()
+                        .contains("Added")
+        );
+    }
+
+    // Update Employee Test
+    @Test
+    void testUpdateEmployeeApi() {
+
+        HttpHeaders headers =
+                new HttpHeaders();
+
+        HttpEntity<String> request =
+                new HttpEntity<>(null, headers);
+
+        ResponseEntity<String> response =
+                restTemplate.exchange(
+                        "http://localhost:" + port +
+                                "/api/employees/1",
+                        HttpMethod.PUT,
+                        request,
+                        String.class
+                );
+
+        assertEquals(
+                HttpStatus.OK,
+                response.getStatusCode()
+        );
+
+        assertTrue(
+                response.getBody()
+                        .contains("Updated")
+        );
+    }
+
+    // Delete Employee Test
+    @Test
+    void testDeleteEmployeeApi() {
+
+        HttpHeaders headers =
+                new HttpHeaders();
+
+        HttpEntity<String> request =
+                new HttpEntity<>(null, headers);
+
+        ResponseEntity<String> response =
+                restTemplate.exchange(
+                        "http://localhost:" + port +
+                                "/api/employees/1",
+                        HttpMethod.DELETE,
+                        request,
+                        String.class
+                );
+
+        assertEquals(
+                HttpStatus.OK,
+                response.getStatusCode()
+        );
+
+        assertTrue(
+                response.getBody()
+                        .contains("Deleted")
+        );
     }
 }
--- employee/src/test/java/com/project/employee/unit/EmployeeServiceTest.java
+++ employee/src/test/java/com/project/employee/unit/EmployeeServiceTest.java
@@ -1,18 +1,34 @@
 package com.project.employee.unit;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import com.project.employee.controller.EmployeeController;
 import com.project.employee.model.Employee;
 import com.project.employee.repository.EmployeeRepository;
 import com.project.employee.service.EmployeeService;
 
 class EmployeeServiceTest {
 
+    private EmployeeService employeeService;
+    private EmployeeController employeeController;
+
+    @BeforeEach
+    void setup() {
+        employeeService = mock(EmployeeService.class);
+        employeeController = new EmployeeController(employeeService);
+    }
+    
     @Test
-    void testGetEmployees() {
+    void testGetEmployees1() {
 
         EmployeeRepository repository =
                 new EmployeeRepository();
@@ -25,4 +41,79 @@ class EmployeeServiceTest {
 
         Assertions.assertEquals(3, employees.size());
     }
+
+    @Test
+    void testHealth() {
+
+        String response = employeeController.health();
+
+        assertEquals(
+                "Employee Application Running ",
+                response
+        );
+    }
+
+    @Test
+    void testGetEmployees() {
+
+        List<Employee> employees = Arrays.asList(
+                new Employee(1, "Anil", "DevOps"),
+                new Employee(2, "Rahul", "Cloud")
+        );
+
+        when(employeeService.getEmployees())
+                .thenReturn(employees);
+
+        List<Employee> result =
+                employeeController.getEmployees();
+
+        assertEquals(2, result.size());
+
+        assertEquals(
+                "Anil",
+                result.get(0).getName()
+        );
+    }
+
+    @Test
+    void testGetEmployeeById() {
+
+        String response =
+                employeeController.getEmployeeById(1);
+
+        assertTrue(response.contains("1"));
+    }
+
+    @Test
+    void testAddEmployee() {
+
+        String response =
+                employeeController.addEmployee();
+
+        assertEquals(
+                "Employee Added Successfully",
+                response
+        );
+    }
+
+    @Test
+    void testUpdateEmployee() {
+
+        String response =
+                employeeController.updateEmployee(1);
+
+        assertTrue(response.contains("1"));
+    }
+
+    @Test
+    void testDeleteEmployee() {
+
+        String response =
+                employeeController.deleteEmployee(1);
+
+        assertTrue(response.contains("1"));
+    }
 }
+
+
+    
